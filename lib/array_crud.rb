f24dc59954077ea17c097f42b5b7763cb390642c
Javaria Brascom <javariab17@gmail.com>
2020-09-16 07:33:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["I","Love","You", 2]
 end
 
 def add_element_to_end_of_array(array, element)
   
+  add_element_to_end_of_array.push("hi")
 end
 
 def add_element_to_start_of_array(array, element)
